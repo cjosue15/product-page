16052b2d9fd70c0c8b94707742d567a9c2cddcee
Carlos Josue <carlosjosuemn98@gmail.com>
2022-05-09 18:41:15 -0500

    feat(comp): add product

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 import ProductViewer from '../components/Product/ProductViewer';
@@ -9,33 +9,62 @@ import { Wrapper } from '../styles/styled-components.styles';
 
 const ProductWrapper = styled.div`
   display: grid;
-  grid-template-columns: minmax(auto, 900px);
+  grid-template-columns: minmax(auto, 950px);
   justify-content: center;
 `;
 
 const ProductColumns = styled.div`
   display: grid;
-  grid-template-columns: repeat(2, 1fr);
+  grid-template-columns: 25rem 1fr;
+  gap: 7rem;
+  align-items: center;
 `;
 
 const ProductSummary = styled.div`
+  /* display: fle; */
+`;
+
+const ModalViewer = styled.div`
+  position: fixed;
+  width: 100%;
+  height: 100%;
+  inset: 0;
+  background-color: rgba(0, 0, 0, 0.75);
+
   display: grid;
+  grid-template-columns: 31.25rem;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Product = () => {
+  const [show, setShow] = useState<boolean>(false);
+
+  const handleModalViewer = (mustShow: boolean) => {
+    setShow(mustShow);
+  };
+
   return (
-    <Wrapper>
-      <ProductWrapper>
-        <ProductColumns>
-          <ProductViewer />
-          <ProductSummary>
-            <ProductInformation />
-            <ProductAmmount />
-            <ProductAdd />
-          </ProductSummary>
-        </ProductColumns>
-      </ProductWrapper>
-    </Wrapper>
+    <>
+      <Wrapper>
+        <ProductWrapper>
+          <ProductColumns>
+            <ProductViewer handleModalViewer={handleModalViewer} isModalViewer={false} />
+            <ProductSummary>
+              <ProductInformation />
+              <ProductAmmount />
+              <ProductAdd />
+            </ProductSummary>
+          </ProductColumns>
+        </ProductWrapper>
+      </Wrapper>
+
+      {show && (
+        <ModalViewer>
+          <ProductViewer handleModalViewer={handleModalViewer} isModalViewer={true} />
+        </ModalViewer>
+      )}
+    </>
   );
 };
 
